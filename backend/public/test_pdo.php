503f3351282f32e4d72203c5186acb41a4740876
Youssef T. Moustafa <yousseftmoustafa@gmail.com>
2024-06-29 21:56:28 +0800

    Submission

--- backend/public/test_pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=lab3database', 'root', '');
-    echo "PDO connection successful!";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
